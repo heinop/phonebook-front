2c24f18287cc9e7d2c56f5cbbb553a6c14f610fb
heinop <pekka.heino@unikie.com>
2020-09-14 12:02:01 +0300

    step3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Person = ({ person }) => {
   return (
-    <p>{person.name}</p>
+  <p>{person.name} {person.number}</p>
   )
 }
 
@@ -11,20 +11,30 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log('Number', event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   const addNewName = (event) => {
     event.preventDefault()
-    const newPerson = {name: newName}
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
     if (persons.map((person) => person.name).includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -34,6 +44,9 @@ const App = () => {
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
