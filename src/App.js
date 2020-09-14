2c50fcc69502a2b6b490496386a6ffb858c5179e
heinop <pekka.heino@unikie.com>
2020-09-14 12:33:27 +0300

    step5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
-
-const Person = ({ person }) => {
-  return (
-  <p>{person.name} {person.number}</p>
-  )
-}
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [ persons, setPersons] = useState([
@@ -54,23 +51,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>
-          filter shown with <input value={filter} onChange={handleFilterChange}/>
-        </div>
-      <h2>add a new</h2>
-      <form onSubmit={addNewName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map((person) => <Person key={person.name} person={person} />)}
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <h3>add a new</h3>
+      <PersonForm 
+        onSubmit={addNewName}
+        newName={newName} handleNameChange={handleNameChange}
+        newNumber={newNumber} handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   )
 
